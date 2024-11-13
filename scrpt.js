72dd75e46a387c7381a2b4682723075ee505aab5
ellisrobertsx <159159321+ellisrobertsx@users.noreply.github.com>
2024-11-13 16:46:02 +0000

    first commit

---
+++ scrpt.js
@@ -0,0 +1,8 @@
+function logIn() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(username, password);
+}
+
+button.addEventListener("click", showButtonStyles);
+
